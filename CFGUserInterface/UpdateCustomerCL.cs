f497b21ebde7cdb9eb52770d8292483e8d3586b2
kelceegabbard <131699890+kelceegabbard@users.noreply.github.com>
2023-04-24 12:26:28 -0500

    Update UpdateCustomerCL.cs

--- CFGUserInterface/UpdateCustomerCL.cs
+++ CFGUserInterface/UpdateCustomerCL.cs
@@ -15,6 +15,32 @@ namespace CFGUserInterface
         public UpdateCustomerCL()
         {
             InitializeComponent();
+            
+        // get user input
+        Console.Write("Enter the name of the person to update: ");
+        string user_input = Console.ReadLine();
+        Console.Write("Enter the new age: ");
+        int new_age = int.Parse(Console.ReadLine());
+
+        // create a connection to the database
+        string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=example;Integrated Security=True";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            // create a command with placeholders
+            string query = "UPDATE people SET age = @age WHERE name = @name";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@age", new_age);
+            command.Parameters.AddWithValue("@name", user_input);
+
+            // open the connection, execute the command, and close the connection
+            connection.Open();
+            int rowsAffected = command.ExecuteNonQuery();
+            Console.WriteLine("Rows affected: " + rowsAffected);
+            connection.Close();
+        }
+    }
+}
+
         }
     }
 }
