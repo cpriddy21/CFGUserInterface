24a40233f9b779660a02a6da718c5b93ffc8201c
Cassandra Priddy <cassandrapriddy21@gmail.com>
2023-04-24 12:35:30 -0500

    .

--- CFGUserInterface/UpdateCustomerCL.Designer.cs
+++ CFGUserInterface/UpdateCustomerCL.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UpdateCustomerCL";
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(419, 120);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 82;
+            dataGridView1.RowTemplate.Height = 41;
+            dataGridView1.Size = new Size(990, 557);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(150, 176);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // UpdateCustomerCL
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1520, 901);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "UpdateCustomerCL";
+            Text = "UpdateCustomerCL";
+            Load += UpdateCustomerCL_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
     }
 }
--- CFGUserInterface/UpdateCustomerCL.cs
+++ CFGUserInterface/UpdateCustomerCL.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace CFGUserInterface
 {
@@ -15,32 +17,37 @@ namespace CFGUserInterface
         public UpdateCustomerCL()
         {
             InitializeComponent();
-            
-        // get user input
-        Console.Write("Enter the name of the person to update: ");
-        string user_input = Console.ReadLine();
-        Console.Write("Enter the new age: ");
-        int new_age = int.Parse(Console.ReadLine());
 
-        // create a connection to the database
-        string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=example;Integrated Security=True";
-        using (SqlConnection connection = new SqlConnection(connectionString))
+            // get user input
+            Console.Write("Enter the name of the person to update: ");
+            string user_input = Console.ReadLine();
+            Console.Write("Enter the new : ");
+            int new_age = int.Parse(Console.ReadLine());
+
+            // create a connection to the database
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=example;Integrated Security=True";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                // create a command with placeholders
+                string query = "UPDATE people SET age = @age WHERE name = @name";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@age", new_age);
+                command.Parameters.AddWithValue("@name", user_input);
+
+                // open the connection, execute the command, and close the connection
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                Console.WriteLine("Rows affected: " + rowsAffected);
+                connection.Close();
+            }
+        }
+
+        private void UpdateCustomerCL_Load(object sender, EventArgs e)
         {
-            // create a command with placeholders
-            string query = "UPDATE people SET age = @age WHERE name = @name";
-            SqlCommand command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@age", new_age);
-            command.Parameters.AddWithValue("@name", user_input);
 
-            // open the connection, execute the command, and close the connection
-            connection.Open();
-            int rowsAffected = command.ExecuteNonQuery();
-            Console.WriteLine("Rows affected: " + rowsAffected);
-            connection.Close();
         }
     }
-}
 
-        }
+}
     }
 }
