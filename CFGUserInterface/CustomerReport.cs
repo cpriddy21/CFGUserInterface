86392cc1c8f6d0e10c96785a35ef5ace3a6e484d
Cassandra Priddy <cassandrapriddy21@gmail.com>
2023-04-26 13:13:48 -0500

    .

--- CFGUserInterface/CustomerReport.cs
+++ CFGUserInterface/CustomerReport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,51 @@ namespace CFGUserInterface
         public CustomerReport()
         {
             InitializeComponent();
+        }
+
+        //Generate a report that displays the total quoted price of all the orders from a given
+        //customer, taking the customer name as input;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "datasource=localhost;port=3306;username=root;password=78jm.Lkk!1lol;database=CFG;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+
+                String CustomerName = textBox1.Text;
+                connection.Open();
+                //string query = Select
+
+                using (MySqlCommand cmd = new MySqlCommand("SELECT c.CustomerName, SUM(ol.QuotedPrice * ol.NumOrdered) AS TotalQuotedPrice FROM Orders o JOIN OrderLine ol ON o.OrderNum = ol.OrderNum JOIN Customer c ON o.CustomerNum = c.CustomerNum JOIN Item i ON ol.ItemNum = i.ItemNum WHERE c.CustomerName = @CustomerName GROUP BY c.CustomerName;", connection))
+                {
+                    cmd.Parameters.AddWithValue("@CustomerName", CustomerName);
+                    using(MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+                
+                /*MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+
+                dataTable.Load(reader);
+
+                dataGridView1.DataSource = dataTable;
+
+
+                reader.Close(); */
+                connection.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- CFGUserInterface/RepresentativeReport.cs
+++ CFGUserInterface/RepresentativeReport.cs
@@ -24,38 +24,41 @@ namespace CFGUserInterface
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "datasource=localhost;port=3306;username=root;password=78jm.Lkk!1lol;database=CFG;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+
+                string query = "SELECT Rep.LastName, Rep.FirstName, COUNT(*) AS NumCustomers, AVG(Customer.Balance) AS AvgBalance " +
+                       "FROM Rep " +
+                       "JOIN Customer ON Rep.RepNum = Customer.RepNum " +
+                       "GROUP BY Rep.LastName, Rep.FirstName " +
+                       "ORDER BY Rep.LastName, Rep.FirstName";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+
+                dataGridView1.DataSource = dataTable;
+
+
+                reader.Close();
+                connection.Close();
+            }
+        }
         /*
 * Generate a report that for each representative, listing the number of customers
-  assigned to the representative and the average balance of the representative’s
-  customers along with the representative's first and last name; 
+assigned to the representative and the average balance of the representative’s
+customers along with the representative's first and last name; 
 */
 
-        /*private void button1_Click(object sender, EventArgs e)
-        
-            string connectionString = "datasource=localhost;port=3306;username=root;password=78jm.Lkk!1lol;database=CFG;";
-            
-            using (MySqlConnection connection = new MySqlConnection(connectionString)) 
-            {
-            
-            string query = "SELECT Rep.LastName, Rep.FirstName, COUNT(*) AS NumCustomers, AVG(Customer.Balance) AS AvgBalance " +
-                   "FROM Rep " +
-                   "JOIN Customer ON Rep.RepNum = Customer.RepNum " +
-                   "GROUP BY Rep.LastName, Rep.FirstName " +
-                   "ORDER BY Rep.LastName, Rep.FirstName";
-            MySqlCommand command = new MySqlCommand(query, connection);
-            MySqlDataReader reader = command.ExecuteReader();
 
-           
-            DataTable dataTable = new DataTable();
-            dataTable.Load(reader);
-            dataGridView1.DataSource = dataTable;
-
-       
-            reader.Close();
-            connection.Close();
-            }
-      
-        } */
 
     }
 }
