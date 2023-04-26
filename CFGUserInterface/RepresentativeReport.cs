9f751a03f4a322e632e881d166c2be341b9049fe
dariacasey <128617643+dariacasey@users.noreply.github.com>
2023-04-26 10:47:10 -0500

    Update RepresentativeReport.cs

--- CFGUserInterface/RepresentativeReport.cs
+++ CFGUserInterface/RepresentativeReport.cs
@@ -16,5 +16,37 @@ namespace CFGUserInterface
         {
             InitializeComponent();
         }
+        
+        // 
+        private void button1_Click(object sender, EventArgs e)
+        {
+      
+            
+            //Copied "..." from credit limit method. dont know if i was supposed to
+            string connectionString = "datasource=localhost;port=3306;username=root;password=78jm.Lkk!1lol;database=CFG;";
+            
+            using (MySqlConnection connection = new MySqlConnection(connectionString)) 
+            {
+            // Execute query
+            string query = "SELECT Rep.LastName, Rep.FirstName, COUNT(*) AS NumCustomers, AVG(Customer.Balance) AS AvgBalance " +
+                   "FROM Rep " +
+                   "JOIN Customer ON Rep.RepNum = Customer.RepNum " +
+                   "GROUP BY Rep.LastName, Rep.FirstName " +
+                   "ORDER BY Rep.LastName, Rep.FirstName";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            // Display results in DataGridView
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            dataGridView1.DataSource = dataTable;
+
+            // Close database connection
+            reader.Close();
+            connection.Close();
+            }
+      
+        }
+
     }
 }
